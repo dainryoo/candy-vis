20444bda929be31a2c5a22342f9f852da899127d
dainryoo <dainryoo@gmail.com>
2019-04-09 19:34:52 -0400

    Added age range slider

--- setup.js
+++ setup.js
@@ -1,8 +1,24 @@
+/* For data processing, use:
+ * sortBy (int)
+ * ascending (boolean)
+ * minAge (int)
+ * maxAge (int)
+ * genderFilter (boolean arr)
+ * countryFilter (boolean arr)
+ */
+
+
 var sortOptions = ['Alphabetical',
                     'Most % Joy',
                     'Most % Despair'];
 var sortBy = 0; // index of what we are currently sorting bar chart by
 var ascending = true;
+
+var dataMinAge = 0; // minimum possible age
+var dataMaxAge = 100; // maximum possible age
+var minAge = 0; // selected min age
+var maxAge = 100; // selected max age
+var editingMin = false;
 
 var genderOptions = ['Male', 'Female', 'Other'];
 var genderFilter = [true, true, true]; // which gender options we currently have selected
@@ -10,8 +26,8 @@ var genderFilter = [true, true, true]; // which gender options we currently have
 var countryOptions = ['USA', 'Canada', 'Other'];
 var countryFilter = [true, true, true]; // which country options we currently have selected
 
-window.onload = start; // call start() when document has finished loading
 
+window.onload = start; // call start() when document has finished loading
 function start() {
     // Build sort controls
     var sort = d3.select('.sort');
@@ -21,7 +37,7 @@ function start() {
         .append('select')
         .on('change', function() {
             sortBy = d3.select(this).property('value');
-            console.log("Now sorting by: " + sortOptions[sortBy]);
+            console.log('Now sorting by: ' + sortOptions[sortBy]);
         })
         .selectAll('option')
         .data(sortOptions)
@@ -32,20 +48,41 @@ function start() {
     // ascending/descending selector
     sort.select('span').append('button')
         .text(function() {
-            return (ascending) ? ("↑") : ("↓");
+            return (ascending) ? ('↑') : ('↓');
         })
         .on('click', function() {
             ascending = !ascending;
-            d3.select(this).text((ascending) ? ("↑") : ("↓"));
-            console.log("Sort is " + ((ascending) ? ("ascending") : ("descending")));
+            d3.select(this).text((ascending) ? ('↑') : ('↓'));
+            console.log('Sort is ' + ((ascending) ? ('ascending') : ('descending')));
         });
 
 
     // Build filter controls
+    var filters;
+    // Age filter
+    filters = d3.select('#age-filter')
+        .append('svg')
+        .append('g');
+    var slider = d3.sliderBottom()
+        .min(dataMinAge)
+        .max(dataMaxAge)
+        .step(1)
+        .width(300)
+        .ticks(5)
+        .default([minAge, maxAge])
+        .fill('#2196f3')
+        .on('onchange', function(val) {
+            minAge = val[0];
+            maxAge = val[1];
+            console.log(minAge + " ~ " + maxAge);
+        });
+    filters.call(slider);
+
+
     // Gender filter
-    var filters = d3.select('#gender-filter');
+    filters = d3.select('#gender-filter');
     filters.append('p')
-        .text("Filter by gender:")
+        .text('Filter by gender:')
     filters.selectAll('label')
         .data(genderOptions)
         .enter()
@@ -58,13 +95,13 @@ function start() {
             .attr('checked', true)
             .on('click', function(d, i) {
                 genderFilter[i] = d3.select(this).property('checked')
-                console.log(genderOptions[i] + ": " + genderFilter[i]);
+                console.log(genderOptions[i] + ': ' + genderFilter[i]);
             });
 
     // Country filter
     var filters = d3.select('#country-filter');
     filters.append('p')
-        .text("Filter by country:")
+        .text('Filter by country:')
     filters.selectAll('label')
         .data(countryOptions)
         .enter()
@@ -77,6 +114,6 @@ function start() {
             .attr('checked', true)
             .on('click', function(d, i) {
                 countryFilter[i] = d3.select(this).property('checked')
-                console.log(countryOptions[i] + ": " + countryFilter[i]);
+                console.log(countryOptions[i] + ': ' + countryFilter[i]);
             });
 }
