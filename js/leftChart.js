82b680902ccfb646fa2ed456f09f6ca3328669b8
dainryoo <dainryoo@gmail.com>
2019-04-12 00:51:37 -0400

    Edited description of website and added title to barchart

--- js/leftChart.js
+++ js/leftChart.js
@@ -8,7 +8,42 @@ function buildLeftChart(data) {
     var bar_height = 10; // height of each individual bar
     var bar_padding = 2; // vertical space between each bar
 
+    var title_top = 25; // top y value of chart title
+    var title_height = 50; // how tall of a space the chart title takes up
+
     svg.attr('height', top_pad + data.length * (bar_height + bar_padding)); // update height of svg depending on number of data items
+
+
+    // title underlines
+    svg.append('g').attr('id', 'bar-chart-title');
+
+    svg.select('#bar-chart-title')
+        .append('line')
+        .attr('id', 'despair-underline')
+        .attr('x1', 239)
+        .attr('y1', title_top+2)
+        .attr('x2', 294)
+        .attr('y2', title_top+2);
+    svg.select('#bar-chart-title')
+        .append('line')
+        .attr('id', 'joy-underline')
+        .attr('x1', 329)
+        .attr('y1', title_top+2)
+        .attr('x2', 353)
+        .attr('y2', title_top+2);
+
+    // title
+    svg.select('#bar-chart-title')
+        .append('text')
+        .text('Reactions of Despair and')
+        .attr('text-anchor', 'end')
+        .attr('transform', 'translate(' + (half_width + hor_pad -4) + ' , ' + title_top +  ')');
+
+    svg.select('#bar-chart-title')
+        .append('text')
+        .text('Joy to Different Candies')
+        .attr('text-anchor', 'start')
+        .attr('transform', 'translate(' +  (half_width + hor_pad) + ' , ' + title_top +  ')');
 
 
     // x-scale for right half (0-100)
@@ -47,7 +82,7 @@ function buildLeftChart(data) {
         .attr('class', 'joy-bar')
         .attr('x', (hor_pad + half_width))
         .attr('y', function(d,i) {
-            return top_pad + i*(bar_height+bar_padding);
+            return title_height + top_pad + i*(bar_height+bar_padding);
         })
         .attr('width', function(d) {
             return x_scale(d.like_perc);
@@ -65,7 +100,7 @@ function buildLeftChart(data) {
             return (hor_pad + half_width) - x_scale(d.dislike_perc);
         })
         .attr('y', function(d,i) {
-            return top_pad + i*(bar_height+bar_padding);
+            return title_height + top_pad + i*(bar_height+bar_padding);
         })
         .attr('width', function(d) {
             return x_scale(d.dislike_perc);
@@ -77,13 +112,13 @@ function buildLeftChart(data) {
     svg.append('g')
         .attr('class', 'x-axis right')
         .call(x_axis_right)
-        .attr('transform', 'translate(' + (hor_pad + half_width) + ', ' + top_pad + ')');
+        .attr('transform', 'translate(' + (hor_pad + half_width) + ', ' + (top_pad  + title_height) + ')');
 
     // add left right-axis
     svg.append('g')
         .attr('class', 'x-axis left')
         .call(x_axis_left)
-        .attr('transform', 'translate(' + hor_pad + ', ' + top_pad + ')');
+        .attr('transform', 'translate(' + hor_pad + ', ' + (top_pad  + title_height) + ')');
 
 
 
